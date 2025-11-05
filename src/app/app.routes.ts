0d28a1f0eb8f3ba7ffb2aded6d5506c6e611300c
shayaankn <shayaankhan11@gmail.com>
2025-11-05 07:12:10 +0530

    update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { Projects } from './projects/projects';
+import { Shotlist } from './shotlist/shotlist';
 
 export const routes: Routes = [
-  { path: '', component: Projects },
+  { path: '', redirectTo: 'projects', pathMatch: 'full' },
+  { path: 'projects', component: Projects },
+  { path: 'shotlist/:id', component: Shotlist },
 ];
