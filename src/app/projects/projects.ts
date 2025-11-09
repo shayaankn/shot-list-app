af87629ecd72a5e469695a1e5984404ef1242a87
shayaankn <shayaankhan11@gmail.com>
2025-11-09 22:25:35 +0530

    make 'projects' component functional

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Storage } from '../services/storage';
+import { FormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, NgFor],
   templateUrl: './projects.html',
   styleUrl: './projects.css',
 })
 export class Projects {
+  newProjectName = '';
+  projects: any[] = [];
 
+  constructor(private storage: Storage, private router: Router) {
+    this.projects = this.storage.getProjects();
+  }
+
+  createProject() {
+    if (!this.newProjectName.trim()) return;
+    const id = Date.now().toString();
+    this.storage.addProject({ id, name: this.newProjectName });
+    this.newProjectName = '';
+    this.projects = this.storage.getProjects();
+  }
+
+  openProject(project: any) {
+    this.router.navigate(['/shotlist', project.id]);
+  }
 }
