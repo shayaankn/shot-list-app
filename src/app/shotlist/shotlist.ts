ca64a3afa1f8fc169bf79aa6993ab2d630041408
shayaankn <shayaankhan11@gmail.com>
2025-11-09 22:26:25 +0530

    make 'shotlist' component functional

--- src/app/shotlist/shotlist.ts
+++ src/app/shotlist/shotlist.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+// import { FormsModule, NgFor } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
+import { Storage } from '../services/storage';
 
 @Component({
   selector: 'app-shotlist',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, NgFor],
   templateUrl: './shotlist.html',
   styleUrl: './shotlist.css',
 })
 export class Shotlist {
+  projectId = '';
+  shots: any[] = [];
 
+  constructor(private route: ActivatedRoute, private storage: Storage) {
+    this.projectId = this.route.snapshot.paramMap.get('id')!;
+    this.shots = this.storage.getShots(this.projectId);
+  }
+
+  addShot() {
+    this.shots.push({ completed: false });
+    this.saveShots();
+  }
+
+  removeShot(i: number) {
+    this.shots.splice(i, 1);
+    this.saveShots();
+  }
+
+  saveShots() {
+    this.storage.saveShots(this.projectId, this.shots);
+  }
 }
